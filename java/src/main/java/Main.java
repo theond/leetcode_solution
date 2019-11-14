75a14b1df61b4a18162512997f2b3943d1b315ec
theond <newdreamx@yeah.net>
2019-11-14 17:42:55 +0800

    增加Java代码

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,8 +1,15 @@
+import solution.array.MaxProfit;
+
 /**
- * @author dfe-team
+ * @author theond
  * desc leetcode
  * date 2019-11-11
  **/
 
 public class Main {
+    public static void main(String[] args){
+        int[] array = new int[]{1,2,3,4,5};
+        int a =MaxProfit.maxProfit(array);
+        System.out.println(a);
+    }
 }
---
+++ java/src/main/java/solution/array/RotateArray.java
@@ -0,0 +1,13 @@
+package solution.array;
+
+/**
+ * @author theond
+ * desc leetcode
+ * date 2019-11-14
+ **/
+
+public class RotateArray {
+    public void rotate(int[] nums, int k){
+        for()
+    }
+}
