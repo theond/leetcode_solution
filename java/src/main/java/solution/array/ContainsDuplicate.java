4be264cd8f4bf7bf1c88694677ef26659f090d6c
theond <newdreamx@yeah.net>
2019-11-15 10:57:00 +0800

    增加ContainsDuplicate

---
+++ java/src/main/java/solution/array/ContainsDuplicate.java
@@ -0,0 +1,37 @@
+package solution.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * @author theond
+ * desc leetcode
+ * date 2019-11-15
+ **/
+
+public class ContainsDuplicate {
+    public static boolean isDuplicate(int[] nums){
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //hash表
+    public boolean isDuplicaeHash(int[] nums){
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i< nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }else{
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
