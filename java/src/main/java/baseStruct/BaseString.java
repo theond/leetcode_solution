ed1aae0e04c85eefa488e52d268f22f8c142e5a3
yux <yux@jrrcapital.com>
2020-10-21 23:15:56 +0800

    增加文件

--- java/src/main/java/baseStruct/BaseString.java
+++ java/src/main/java/baseStruct/BaseString.java
@@ -4,10 +4,13 @@ public class BaseString {
     public static void main(String[] args) {
 
         //创建字符串
+        String s1 = new String();
+        String s2 = "improvegogogo";
 
         //返回字符串长度
-
+        int s2Len = s2.length();
         //字符替换
+        String s3 = s2.replace("o","s");
 
         //字符串切割
 
---
+++ python/baseStruct/BaseArray.py
@@ -0,0 +1,8 @@
+# 创建字符串
+s1 = str()
+s2 = "improvegogogo"
+#字符串长度
+s2Len = len(s2)
+# print(s2Len)
+# 切割字符串
+subS2 = s2[-3:] # 等同于s2[10:13]
