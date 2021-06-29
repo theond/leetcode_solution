39826564bad312c5a4e3baeb17d9e179bfa1b2b1
yuxin5 <yuxin5@genomics.cn>
2021-06-29 13:54:05 +0800

    增加cpp

---
+++ cpp/cpp/base_sort/BubbleSort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "BubbleSort.h"
+using namespace std;
+
+BubbleSort::BubbleSort(){}
+
+void BubbleSort::bubbleSort(int input[], int length) {
+	bool swaped = true;
+	for (int i = 0; i < length && swaped; i++) {
+		swaped = false;
+		for (int j = 1; j < length - i; j++) {
+			if (input[j-1] > input[j]) {
+				swap(input[j-1], input[j]);
+				swaped = true;
+			}
+		}
+	}
+	for (int a = 0; a < length; a++){
+		cout << input[a] << endl;
+	}
+	
+}
+
+void BubbleSort::swap(int &a, int &b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+int main() {
+	BubbleSort sort;
+	int input[7] = {10, 21, 9, 7, 1, 42, 37};
+	int length = sizeof(input) / sizeof(input[0]);
+	sort.bubbleSort(input, length);
+}
---
+++ cpp/cpp/base_sort/BubbleSort.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef BUBBLESORT
+
+#define BUBBLESORT
+class BubbleSort 
+{
+private:
+	void swap(int& a, int& b);
+public:
+	BubbleSort();
+	void bubbleSort(int input[], int length);
+
+};
+#endif // !BUBBLESORT
+
