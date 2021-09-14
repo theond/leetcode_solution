d3eb7f3b4060d129d55f499a19700031d1bf61fc
yuxin5 <yuxin5@genomics.cn>
2021-09-14 14:15:44 +0800

    add find min

---
+++ cpp/cpp/array/FindRotateMin.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef FINDROTATEMIN
+class FindRotateMin {
+private:
+	
+public:
+	FindRotateMin();
+	int findRotateMin(int nums[], int length);
+
+	void findMin(int nums[], int l, int r);
+};
+#endif // !FINDROTATEMIN
