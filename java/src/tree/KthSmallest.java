ef106a148240ab0a97987e344d02f141c80b27c1
yuxin5 <yuxin5@genomics.cn>
2022-03-23 14:37:07 +0800

    Kth smallest

---
+++ java/src/tree/KthSmallest.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class KthSmallest {
+    public int findKthNumber(int n, int k) {
+        int[] array = new int[n];
+        for(int i = 0 ; i < n; i++){
+            
+        }
+    }
+}
