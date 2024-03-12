38db89607752a6af39d3ee85ca8360dff3dd9871
yuxin <yuxin5@bgi.com>
2024-03-12 10:50:53 +0800

    add go code

---
+++ go/arrays/removeElement/removeElement.go
@@ -0,0 +1,13 @@
+package removeelement
+
+/*
+ * https://leetcode.cn/problems/remove-element
+ */
+func removeElement(nums []int, val int) int {
+	count, j := 0, len(nums)-1
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == val {
+			temp := nums[j]
+		}
+	}
+}
